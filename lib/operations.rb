bb4c475e56e0b8a4b4ee01cfd2239c9e71ca7586
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-23 20:24:13 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
 def unsafe?(speed)
-
+if speed < 40 
+  true  
+  elsif 
+  speed > 60
+  true
+else
+  false 
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed < 40 ? false : true
+	if speed > 60 ? false : true
+	else 
+	  true
 end
 	
 
